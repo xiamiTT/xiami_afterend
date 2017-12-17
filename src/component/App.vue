2a133c685ab08418be1f3c842d1510a3f30c5c35
xiami <xiami@qq.com>
2017-12-17 20:01:05 +0800

    vue项目脚手架搭建

---
+++ src/component/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div>方碑大街</div>
+</template>
+
+<script>
+// 导出组件配置
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+
+import AppComponent from './component/App.vue';
+
+
+
+new Vue({
+    el: '#app',
+
+    render: c => c(AppComponent)
+})
